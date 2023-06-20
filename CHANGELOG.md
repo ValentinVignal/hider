60b43eca3abc4edf0b750fcf485a1bb29fc29eee
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-20 21:28:02 +0800

    :sparkles: Order the item alphabetically

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## Unreleased
+
+- Upgrades to flutter 3.10.5.
+- Orders the sub items alphabetically.
--- lib/services/item_model.dart
+++ lib/services/item_model.dart
@@ -3,23 +3,40 @@ import 'package:hider/services/item.dart';
 import 'package:hider/utils/path.dart';
 import 'package:riverpod/riverpod.dart';
 
+/// The item from the database.
 final originalItemProvider =
     StreamProvider.autoDispose.family<Item, HiderPath>((ref, path) {
   return FirestoreItemService.watch(path);
 });
 
-final itemProvider =
-    StateProvider.autoDispose.family<Item, HiderPath>((ref, path) {
-  final value = ref.watch(originalItemProvider(path)).value;
-  if (value != null) return value;
-  return Item.empty(path.isNotEmpty ? path.last : '');
-});
+/// The database in the front end which is potentially edited.
+final itemProvider = StateProvider.autoDispose.family<Item, HiderPath>(
+  (ref, path) {
+    final value = ref.watch(originalItemProvider(path)).value;
+    if (value != null) return value;
+    return Item.empty(path.isNotEmpty ? path.last : '');
+  },
+  dependencies: [originalItemProvider],
+);
 
-final subItemsProvider =
+/// The items from the database.
+final _subItemsProvider =
     StreamProvider.autoDispose.family<Iterable<Item>, HiderPath>(
   (ref, path) {
     return FirestoreItemService.watchSubs(path);
   },
+);
+
+/// The ordered items in the front end.
+final subItemsProvider =
+    Provider.autoDispose.family<AsyncValue<Iterable<Item>>, HiderPath>(
+  (ref, path) {
+    return ref.watch(_subItemsProvider(path)).whenData((items) {
+      return [...items]
+        ..sort((itemA, itemB) => itemA.name.compareTo(itemB.name));
+    });
+  },
+  dependencies: [_subItemsProvider],
 );
 
 final allItemsOfPathProvider =
